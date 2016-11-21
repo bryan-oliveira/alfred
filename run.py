2bbfec45b7ca01e7e4fcb74e0f8d00385b7ae972
Bryan Oliveira <vicdaruf@yahoo.com>
2016-11-21 18:44:39 +0000

    Fixed project configs for aws server deployment

--- run.py
+++ run.py
@@ -3,4 +3,5 @@ import os
 
 port = int(os.environ.get('PORT', 5000))
 
-app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port, debug=True)
