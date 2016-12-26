e739281a3ff768c9a91e449186c165f5abc44a8d
Bryan Oliveira <vicdaruf@yahoo.com>
2016-12-26 19:18:01 +0000

    Added spice recognition

--- app/database/ingredients/create_ingredient_db.py
+++ app/database/ingredients/create_ingredient_db.py
@@ -1,19 +1,21 @@
-"""
-This module parses USDA food files. Food ingredients are then saved to json format.
-"""
-
-from config import USDA_FRUIT_DATA, USDA_VEGETABLE_DATA, VEGETABLE_DB, FRUIT_DB
+from config import USDA_FRUIT_DATA, USDA_VEGETABLE_DATA, USDA_SPICES_DATA, \
+    VEGETABLE_DB, FRUIT_DB, SPICES_DB
 from file_operations import is_empty_file
 import json
 import sys
 
+"""
+This module parses USDA food files. Food ingredients are saved to json format consisting
+of only the ingredients themselves.
+"""
 
 food_types = {"vegetables": [USDA_VEGETABLE_DATA, VEGETABLE_DB],
-              "fruits": [USDA_FRUIT_DATA, FRUIT_DB]
+              "fruits": [USDA_FRUIT_DATA, FRUIT_DB],
+              "spices": [USDA_SPICES_DATA, SPICES_DB]
               }
 
 
-def create_DB_Of_FoodType(food_type):
+def create_db_Of_FoodType(food_type):
     if food_type not in food_types:
         return False, "Food type not found."
 
@@ -28,7 +30,24 @@ def create_DB_Of_FoodType(food_type):
             count = 0
 
             for item in items:
-                curr = item['name'].split(',')[0].lower()
+
+                # Spices file needs slightly different parsing technique
+                if food_type == 'spices':
+
+                    # Spices file categorizes some spices as "Spices, <spice name>, <spice attributes>"
+                    # others directly as "<spice name>, <spice attributes>"
+                    if item['name'].split(',')[0].lower() == 'spices':
+                        curr = item['name'].split(',')[1].lower()
+                    else:
+                        curr = item['name'].split(',')[0].lower()
+
+                # Other USDA files categorize as "<ingredient>, <ingredient attributes>"
+                else:
+                    curr = item['name'].split(',')[0].lower()
+
+                # Strip whitespaces
+                curr = curr.strip()
+                print curr
 
                 # Discard repeat vegetable names
                 if curr not in food_list:
@@ -42,9 +61,6 @@ def create_DB_Of_FoodType(food_type):
                 elif curr[-1] == 's' and curr[:-1] not in food_list:
                     food_list.append(curr[:-1])
 
-            # [#] print>> sys.stderr, "Total ingredients:", data['list']['total'], "Unique", count
-            # [#] print>> sys.stderr, food_list
-
             with open(food_types[food_type][1], 'w') as f2:
                 json.dump(food_list, f2)
                 # [#] print>> sys.stderr, "Wrote %s db to disk." % food_types[food_type][1]
@@ -52,7 +68,8 @@ def create_DB_Of_FoodType(food_type):
     return False
 
 if __name__ == '__main__':
-    # create_DB_Of_FoodType("fruits")
-    # create_DB_Of_FoodType("vegetables")
+    # create_db_Of_FoodType('fruits')
+    # create_db_Of_FoodType('vegetables')
+    create_db_Of_FoodType('spices')
     pass
 
